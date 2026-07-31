71b2a0298b20531fbbf7996137e9e2724a936a77
Thamid Hasnine <thamid819@gmail.com>
2026-08-01 01:24:45 +0600

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card"></div>
+        <h1>👋 Welcome</h1>
+        <h2>Thamid Hasnine</h2>
+        <h3>AUST CSE</h3>
+        <p>React Developer 🚀</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
