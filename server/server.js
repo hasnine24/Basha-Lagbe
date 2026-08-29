74bb11e1409afb5f8fb7c84e8e10196f2d83cc1d
Tahmid <tahmid@Tahmids-MacBook-Air.local>
2026-08-30 05:57:41 +0600

    Set up backend

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Basha Lagbe API is running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server running on port 5000");
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed:");
+    console.log(error.message);
+  });
