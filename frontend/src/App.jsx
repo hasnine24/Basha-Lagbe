83ac1d878654848a1f476d8c4f91c06ae5e7140a
Tahmid <tahmid@Tahmids-MacBook-Air.local>
2026-09-13 12:19:29 +0600

    Fix scroll position on page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 
 import Home from "./pages/tahmid/Home";
@@ -13,26 +14,39 @@ import EditPropertyPage from "./pages/hasnine/edit_property_page";
 import PrivateRoute from "./utils/PrivateRoute";
 import PublicRoute from "./utils/PublicRoute";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <Route element={<PublicRoute />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Route>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
 
-      <Route element={<PrivateRoute />}>
-        <Route path="/profile" element={<Profile />} />
-      </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
 
-      <Route path="/properties" element={<Properties />} />
-      <Route path="/properties/:id" element={<ProductPage />} />
-      <Route path="/add-property" element={<AddPropertyPage />} />
-      <Route path="/edit-property" element={<EditPropertyPage />} />
-      <Route path="/edit-property/:id" element={<EditPropertyPage />} />
-    </Routes>
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/properties/:id" element={<ProductPage />} />
+        <Route path="/add-property" element={<AddPropertyPage />} />
+        <Route path="/edit-property" element={<EditPropertyPage />} />
+        <Route path="/edit-property/:id" element={<EditPropertyPage />} />
+      </Routes>
+    </>
   );
 }
 
